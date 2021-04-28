6dd941219f17475d43cfbdf5f0f3a645c8f9ce9e
Sneha sadananda <83263420+snehasadananda@users.noreply.github.com>
2021-04-29 00:12:31 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function tooglebutton(){
+    navList.ClassList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click' togglebutton)
